919d094c2f664b95169ac1b4591c9f2ff20cf934
Nishitagyanwani <87478323+Nishitagyanwani@users.noreply.github.com>
2021-07-30 19:57:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var sea;
+var ship;
+
+function preload(){
+seaimage=loadImage("sea.png");
+shipimage=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+}
+
+function setup(){
+  createCanvas(400,400)
+
+  sea=createSprite(400,200);
+  sea.addImage(seaimage);
+  sea.velocityX=-5;
+  sea.scale=0.3;
+
+  ship=createSprite(130,200,30,30);
+  ship.addAnimation("moving ship",shipimage);
+  ship.scale=0.25;
+}
+
+function draw() {
+  background("blue");
+
+  if(sea.x < 0){
+    sea.x=sea.width/8;
+  }
+drawSprites();
+}
+
+
+
+
+
+
+
+
+
